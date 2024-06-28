f0551bc68866190d75211b60314d115ec44c6c0a
Andres Kasekamp <andreskasekamp@gmail.com>
2024-06-28 11:11:42 +0300

    reamde

--- rabbitmq-start.sh
+++ rabbitmq-start.sh
@@ -4,8 +4,11 @@ export RABBIT_USER
 export RABBIT_PASS
 
 # Create Rabbitmq user asynchronously and subshell
-(rabbitmqctl wait /var/run/rabbitmq/pid --timeout 15; \
-rabbitmqctl add_user $RABBIT_USER $RABBIT_PASS 2>/dev/null ; \
-rabbitmqctl set_user_tags $RABBIT_USER administrator ; \
-rabbitmqctl set_permissions -p / $RABBIT_USER  ".*" ".*" ".*" ; \
-echo "User $RABBIT_USER has been created") & rabbitmq-server
+(
+    rabbitmqctl wait --timeout 15 $RABBITMQ_PID_FILE
+    rabbitmqctl add_user $RABBIT_USER $RABBIT_PASS 2>/dev/null
+    rabbitmqctl set_user_tags $RABBIT_USER administrator
+    rabbitmqctl set_permissions -p / $RABBIT_USER ".*" ".*" ".*"
+    echo "User $RABBIT_USER has been created"
+) &
+rabbitmq-server
